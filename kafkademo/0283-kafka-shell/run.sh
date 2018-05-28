f61f0491bd23e69bb917630d16a5504b6eafa2fa
peach <137318793>
2018-05-29 00:54:48 +0800

    新增SparkStreaming读取Kafka数据写入HBase示例，生产消息示例代码

---
+++ kafkademo/0283-kafka-shell/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#########################################
+# 创建Topic
+# kafka-topics --create --zookeeper cdh01.fayson.com:2181,cdh02.fayson.com:2181,cdh03.fayson.com:2181 --replication-factor 3 --partitions 3 --topic kafka_sparkstreaming_hbase_topic
+#
+########################################
+
+JAVA_HOME=/usr/java/jdk1.8.0_131
+
+#要读取的文件
+read_file=$1
+
+for file in `ls lib/*jar`
+do
+    CLASSPATH=$CLASSPATH:$file
+done
+export CLASSPATH
+
+${JAVA_HOME}/bin/java -Xms1024m -Xmx2048m com.cloudera.nokerberos.ReadUserInfoFIleToKafka $read_file
