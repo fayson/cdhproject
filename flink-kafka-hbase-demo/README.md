bf1e54a6e887a71c290a879e88428b24ac804252
fayson <137318793@qq.com>
2020-03-24 21:51:13 +0800

    增加Flink读非Kerberos环境下的Kafka写入Kerberos环境下的HBase集群

--- flink-kafka-hbase-demo/README.md
+++ flink-kafka-hbase-demo/README.md
@@ -43,3 +43,45 @@ flink run -m yarn-cluster -yn 3 -yjm 1024 -ytm 1024 \
 --hostname 192.168.0.234 \
 --port 19000
 ```
+
+
+### 二、Kafka2HBaseSample 示例说明
+
+Flink2HBaseSample 示例主要是通过实时读取非Kerberos环境Kafka数据,使用CountWindow创建将数据批量的转换为List\<Put>，再通过自定义的Sink实时的写入Kerberos环境的HBase
+
+- 在Kafka集群创建一个测试用的Topic
+```shell script
+kafka-topics --create --zookeeper 192.168.0.221:2181,192.168.0.222:2181,192.168.0.223:2181 --replication-factor 3 --partitions 3 --topic test_topic
+
+kafka-console-producer --broker-list 192.168.0.221:9092,192.168.0.222:9092,192.168.0.223:9092 --topic test_topic
+
+kafka-console-consumer --topic test_topic --from-beginning --bootstrap-server 192.168.0.221:9092,192.168.0.222:9092,192.168.0.223:9092
+
+```
+
+- 创建HBase表并授予测试用户对表的操作权限
+```shell script
+[root@cdh1 ~]# kinit hbase/admin
+Password for hbase/admin@PREST.COM: 
+[root@cdh1 ~]# hbase shell                                                                                                                
+hbase(main):001:0> grant 'cdhadmin','RWCXA'
+Took 0.6101 seconds                                                                                                                   
+hbase(main):002:0> create 'flink_hbase','info'
+Created table flink_hbase
+Took 1.5389 seconds                                                                                                                   
+=> Hbase::Table - flink_hbase
+hbase(main):004:0> scan 'flink_hbase'
+ROW                                COLUMN+CELL                                                                                        
+0 row(s)
+Took 0.2107 seconds                                                                                                                   
+hbase(main):005:0> 
+
+```
+
+- 在Flink集群的Gateway节点上执行如下命令向集群提交Flink作业
+```shell script
+flink run -m yarn-cluster -yn 3 -yjm 1024 -ytm 1024 \
+-yD security.kerberos.login.keytab=/opt/cloudera/keytabs/cdhadmin.keytab \
+-yD security.kerberos.login.principal=cdhadmin  \
+--class com.fayson.Kafka2HBaseSample flink-kafka-hbase-demo-1.0-SNAPSHOT-jar-with-dependencies.jar
+```
--- flink-kafka-hbase-demo/src/main/java/com/fayson/Kafka2HBaseSample.java
+++ flink-kafka-hbase-demo/src/main/java/com/fayson/Kafka2HBaseSample.java
@@ -1,57 +1,46 @@
 package com.fayson;
 
 import com.fayson.customsink.HBaseSink;
-import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * package: com.cloudera.hbase
- * describe: Flink从Socket读数据，将读取的数据解析后写入HBase示例
+ * describe: Flink从非Kerberos的Kafka集群读取数据，写入Kerberos环境下的HBase集群
  * creat_user: Fayson
  * email: htechinfo@163.com
- * creat_date: 2020/03/2020/3/19
- * creat_time: 9:40 下午
+ * creat_date: 2020/03/2020/3/24
+ * creat_time: 10:14 上午
  * 公众号：Hadoop实操
  */
-public class Flink2HBaseSample {
-
+public class Kafka2HBaseSample {
     private static String tableName = "flink_hbase";
     private static final String columnFamily = "info";
 
     public static void main(String[] args) throws Exception {
-
-
-
-        final String hostname;
-        final int port;
-        try {
-            final ParameterTool params = ParameterTool.fromArgs(args);
-            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
-            port = params.getInt("port");
-        } catch (Exception e) {
-            System.err.println("No port specified. Please run 'SocketWindowWordCount " +
-                    "--hostname <hostname> --port <port>', where hostname (localhost by default) " +
-                    "and port is the address of the text server");
-            System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
-                    "type the input text into the command line");
-            return;
-        }
-
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        System.out.println("hostname:" + hostname + ", port:" + port);
 
-        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.0.221:9092,192.168.0.222:9092,192.168.0.223:9092");
+        properties.setProperty("group.id", "testGroup");
+        properties.setProperty("auto.offset.reset", "earliest");
+        properties.setProperty("security.protocol", "PLAINTEXT");
 
-        DataStream<List<Put>> stream_d = text.countWindowAll(3).apply(new AllWindowFunction<String, List<Put>, GlobalWindow>() {
+        DataStream<String> stream = env
+                .addSource(new FlinkKafkaConsumer<>("test_topic", new SimpleStringSchema(), properties));
+
+        DataStream<List<Put>> stream_d = stream.countWindowAll(3).apply(new AllWindowFunction<String, List<Put>, GlobalWindow>() {
             @Override
             public void apply(GlobalWindow globalWindow, Iterable<String> message, Collector<List<Put>> out) throws Exception {
                 List<Put> putList=new ArrayList<Put>();
@@ -69,8 +58,6 @@ public class Flink2HBaseSample {
 
         stream_d.addSink(new HBaseSink(tableName)).setParallelism(4);
 
-        env.execute("Flink2HBaseSample");
-
+        env.execute("Kafka2HBaseSample");
     }
-
 }
